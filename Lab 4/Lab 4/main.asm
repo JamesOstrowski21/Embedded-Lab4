6615f64bc0c46eb16c47f18cf7b1f798d0845d7d
KaiLindholm <kailindholm20022@gmail.com>
2023-04-04 13:38:42 -0500

    done?

--- Lab 4/Lab 4/main.asm
+++ Lab 4/Lab 4/main.asm
@@ -30,12 +30,13 @@
 .def temp = r18
 .def value = r20		; Current value of rpg, used to calc duty cycle
 .def input = r22		; State of the rpg 	
+.def prevInput = r21
 .def fanBool = r19		; state of the fan 
-.def rpgStates = r17
 .def update = r24
 .def delayLength = r23
 .def rpgInterruptCount = r25
 .def prevValue = r15
+.def tempUpdate = r17
 rjmp RESET
 
 msg1: .db "DC = ", 0x00
@@ -51,7 +52,8 @@ RESET:
 	out		SPH, temp
 	ldi		fanBool, 0			; fans initial state is off
 	ldi		update, 0
-
+	clr		prevValue
+	ldi		prevInput, 0x03		; inital state of RPG is 11
 ;---------------------PIN CONFIGURATION-------------------------
 	clr		r16							; reset r16
 	ldi		r16, (1<<DDB3) | (1<<DDB5)	; set pins 3 and 5 as outputs for the LCD
@@ -70,13 +72,16 @@ RESET:
 ;--------------------Setup pin interrupts------------------------	
 	// PCINT0-1 - RPG interrupts
 	// INT0	- PB  interrupt
-	ldi		temp, (1<<INT0)
-	out		EIMSK, temp
-	ldi		temp, 0x02
+	ldi		temp, 0x02		; enable INT0 interrupts
 	sts		EICRA, temp
+
+	ldi		temp, (1<<INT0)	; enables int0 flag. 
+	out		EIMSK, temp
+
 
 	ldi		temp, (1<<PCIE0)
 	sts		PCICR, temp
+
 	ldi		temp, (1<<PCINT0) | (1<<PCINT1)
 	sts		PCMSK0, temp
 
@@ -90,32 +95,28 @@ RESET:
 	ldi		temp, 200									; top value, this allows 0-199 steps in duty cycle. or 200 total steps 
 	sts		OCR2A, temp			
 			
-	ldi		value, 156									; Set the starting duty cycle
+	ldi		value, 160									; Set the starting duty cycle
 	mov		temp, value
 	sts		OCR2B, temp
-
-	;ldi		temp, (1<<OCIE2A)| (1<<OCIE2B) | (1 << TOIE2)	; enable T/C2 interrupts 
-	;sts		TIMSK2, temp
 
 ;--------------Enable T/C0 for delays-------------------------------------
 	ldi		temp, (1<<CS01) | (1<<CS00)
 	out		TCCR0B, temp	; Timer clock = system clock / 64
-	rcall LCDinit
 
-
-sei
+rcall LCDinit
 rcall updateDC
 rcall fanON
-	
+
+sei
 main: 
 	sbrc	update, 0
 	rjmp	ToggleFan
-	sbrc	update, 1 
-	rjmp	incrementDC 
-	sbrc	update, 2 
+	sbrc	update, 1
+	rjmp	incrementDC
+	sbrc	update, 2
 	rjmp	decrementDC
 	
-	rjmp main
+	rjmp	main
 INT0_vect: 
 	ldi		temp, SREG		; save state of SREG
 	push	temp
@@ -126,7 +127,6 @@ INT0_vect:
 	eor		fanBool, temp
 
 	rjmp	intDONE			 
-
 TOV0_INT:
 	ldi		temp, SREG		; save state of SREG
 	push	temp
@@ -136,105 +136,116 @@ TOV0_INT:
 	rjmp	intDONE		
 		 
 PCINT0_INT:
-	cli 
-	ldi		input, PINB		; read state of PINB
+	ldi		temp, SREG		; save state of SREG
+	push	temp
+	cli
+	in		input, PINB
 	andi	input, 0x03		; mask out PINA and PINB
-	or		rpgStates, input
-	lsl		rpgStates
-	lsl		rpgStates
 
-	inc		rpgInterruptCount 
-	cpi		rpgInterruptCount, 4
-	breq	rpgUpdate
-	
+	inc		rpgInterruptCount
+	cpi		rpgInterruptCount, 1
+	breq	firstTime
+
+	cpi		rpgInterruptCount, 4 
+	breq	DONE
 	rjmp	intDONE
 
-rpgUpdate: 
-	sbrs	rpgStates, 7
-	ori		update, (1<<1)	; 
-	ori		update, (1<<2)	; 
+DONE: 
+	;mov		update, tempUpdate
+	clr		rpgInterruptCount
+	rjmp	intDONE
+
+firstTime:
+	cpi		input, 0x01
+	breq	rpgCW
+	cpi		input, 0x02
+	breq	rpgCCW
+
+	rjmp	intDONE
+
+rpgCW: 
+	ldi		update, 0x02
+	rjmp	intDONE
+rpgCCW: 
+	ldi		update, 0x04
 	rjmp	intDONE
 
 incrementDC:
-	ldi		temp, 0x00		; turn RPG interrupts off
-	sts		PCMSK0, temp
+	cli
+	clr		update
 
-	ldi		temp, 5
+	cpi		value, 200
+	breq	incDone
+
+	ldi		temp, 2
 	add		value, temp
-	// determine digits to display
-	;rcall	updateDisplay
-	
-	ldi		update, (0<<1)	; set the second bit to a 0 
-
-	ldi		temp, 0x03		; turn RPG interrupts back on
-	sts		PCMSK0, temp
-
+incDone:
+	sts		OCR2B, value		
+	// update DC 
+	cli
+	rcall	updateDC
+	rcall	fanON
+	sei
 	rjmp	main
-
 
 decrementDC:
-	ldi		temp, 0x00		; turn RPG interrupts off
-	sts		PCMSK0, temp
+	cli
+	clr		update
 
-	ldi		temp, 5
+	cpi		value, 0
+	breq	decDone
+
+	ldi		temp, 2
 	sub		value, temp
+decDone: 
+	sts		OCR2B, value	
+		
 	// update DC
-;	rcall	updateDisplay
-	
-	ldi		update, (0<<2)	; set the third bit to a 0 
-	
-	ldi		temp, 0x03		; turn RPG interrupts back on
-	sts		PCMSK0, temp
-	
+	cli 
+	rcall	updateDC
+	rcall	fanON
+	sei
 	rjmp	main
+
 		  
 ToggleFan:
-	cli
-	ldi update, 0
+	clr		update
 	cpi fanBool, 1 
 	breq turnFanOn
 	cpi fanbool, 0
 	breq turnFanOff
-	sei
-	jmp main
+
+	rjmp main
 
 turnFanOff:
-	cbi		PORTB, PB4 
-	ldi		temp, 0
-	mov		prevValue, value 
-	ldi		value, 30
-	rcall	fanOFF
-	rcall	displayDC
-	sts		OCR2B, temp		
-	clr		temp
-	ldi		temp, 0x00		; turn RPG interrupts off. Keep button interrupts
-	sts		PCMSK0, temp
+	mov		prevValue, value  ; store the current state of value
+
+	ldi		value, 0
+	cli
+	rcall	updateDC			; update DC with a value of 0 
+	rcall	fanOFF				; add fan off on second line 
 	sei
+	
+	sts		OCR2B, value			; turn fan off by setting duty cycle to 0 
+	mov		value, prevValue	; move back value
+
+	ldi		temp, 0x00		; turn RPG interrupts off. Keep button interrupts.
+	sts		PCMSK0, temp
+
 	rjmp	main
 
 turnFanOn: 
-	sbi		PORTB, PB4
+	cli
+	rcall	updateDC			; set back the duty cycle from value. 
+	rcall	fanON
+	sei 
+		
+	sts		OCR2B, value		
+
 	ldi		temp, 0x03		; turn RPG control back on
 	sts		PCMSK0, temp
-	mov		value, prevValue
-	rcall	displayDC
-	rcall	fanON
-	cpi		value, 50		; check if DC > 25%
-	rcall	rampUp
-	sei
+
 	rjmp	main
-
-
-rampUp: 
-	ldi		temp, 200		; ramp fan up to 25% DC 
-	sts		OCR2B, temp		
-	nop
-	nop
-	nop
-	sts		OCR2B, value		; bring fan speed back to previous state	
-	
-	sei
-	ret
 
 intDONE:
 	pop temp
@@ -243,6 +254,31 @@ intDONE:
 	reti
 
 updateDC:
+	cbi PORTB, PB5
+	ldi r17, 0x00
+	out PORTC, r17
+	nop
+	rcall LCDStrobe
+	rcall delay_ms
+
+	ldi r17, 0x01
+	out PORTC, r17
+	nop
+	rcall LCDStrobe
+	rcall delay_ms
+
+	ldi r17, 0x00
+	out PORTC, r17
+	nop
+	rcall LCDStrobe
+	rcall delay_ms
+
+	ldi r17, 0x02
+	out PORTC, r17
+	nop
+	rcall LCDStrobe
+	rcall delay_ms
+
 	sbi PORTB, PB5
 	ldi r24, 5
 	ldi r30,LOW(2*msg1) ; Load Z register low
@@ -261,7 +297,7 @@ fanON:
 	ldi r30,LOW(2*msg2) ; Load Z register low
     ldi r31,HIGH(2*msg2) ; Load Z register high
 	rcall displayCString
-	rcall Firstline
+	;rcall Firstline
 	ret
 
 fanOFF:
@@ -270,11 +306,18 @@ fanOFF:
 	ldi r30,LOW(2*msg3) ; Load Z register low
     ldi r31,HIGH(2*msg3) ; Load Z register high
 	rcall displayCString
-	rcall Firstline
+	;rcall Firstline
 	ret
 
 Firstline:
 	cbi PORTB, PB5
+	ldi r17, 0x08
+	out PORTC, r17
+	nop
+	rcall LCDStrobe
+	rcall delay_ms
+	rcall delay_ms
+
 	ldi r17, 0x00
 	out PORTC, r17
 	nop
@@ -282,12 +325,6 @@ Firstline:
 	rcall delay_ms
 	rcall delay_ms
 
-	ldi r17, 0x02
-	out PORTC, r17
-	nop
-	rcall LCDStrobe
-	rcall delay_ms
-	rcall delay_ms
 	ret
 
 Secondline:
@@ -328,6 +365,68 @@ LCDStrobe:
 	rcall delay_ms
 	rcall delay_ms
 	cbi PORTB, PB3
+	ret
+
+displayDC:
+.dseg 
+	dtxt: .BYTE 4 ;allocation
+	twoDigit: .BYTE 3
+.cseg
+	mov dd8u, value
+	ldi dv8u, 2
+	rcall div8u
+
+	cpi dres8u, 100
+	breq onehundred
+
+	ldi r29, 0x00
+	sts	twoDigit+2, r29	; load in terminating null byte
+
+	; ones place
+	ldi dv8u, 10
+	rcall div8u			
+	ldi r29, 0x30
+	add drem8u, r29
+	sts twoDigit+1, drem8u
+	
+	; tens place 
+	rcall div8u
+	add drem8u, r29
+	sts twoDigit, drem8u
+	rjmp read
+
+onehundred:
+	ldi		r29, 0x00
+	sts		dtxt+3, r29
+	ldi		temp, 0x30
+	sts		dtxt+2, temp
+	sts		dtxt+1, temp
+	ldi		temp, 0x31
+	sts		dtxt, temp
+
+readhundred:
+	ldi r30, LOW(dtxt)
+	ldi r31, HIGH(dtxt)
+	rjmp displayDString
+
+read:
+	ldi r30, LOW(twoDigit)
+	ldi r31, HIGH(twoDigit)
+	rjmp displayDString
+
+displayDstring:
+	ld r0,Z+
+	tst r0 ; Reached end of message ?
+	breq done_dsd ; Yes => quit
+	swap r0 ; Upper nibble in place
+	out PORTC,r0 ; Send upper nibble out
+	rcall LCDStrobe ; Latch nibble 
+	swap r0 ; Lower nibble in place
+	out PORTC,r0 ; Send lower nibble out
+	rcall LCDStrobe ; Latch nibble
+	rjmp displayDString
+
+done_dsd:
 	ret
 
 LCDinit:
@@ -422,70 +521,7 @@ LCDinit:
 	nop
 	rcall LCDStrobe
 	rcall delay_ms
-	ret
-
-displayDC:
-.dseg 
-	dtxt: .BYTE 4 ;allocation
-	twoDigit: .BYTE 3
-.cseg
-	mov dd8u, value
-	ldi dv8u, 2
-	rcall div8u
-
-	cpi dres8u, 100
-	breq onehundred
-
-	ldi r29, 0x00
-	sts	twoDigit+2, r29	; load in terminating null byte
-
-	; ones place
-	ldi dv8u, 10
-	rcall div8u			
-	ldi r29, 0x30
-	add drem8u, r29
-	sts twoDigit+1, drem8u
-	
-	; tens place 
-	rcall div8u
-	add drem8u, r29
-	sts twoDigit, drem8u
-	rjmp read
-
-onehundred:
-	ldi		r29, 0x00
-	sts		dtxt+3, r29
-	ldi		temp, 0x30
-	sts		dtxt+2, temp
-	sts		dtxt+1, temp
-	ldi		temp, 0x31
-	sts		dtxt, temp
-
-readhundred:
-	ldi r30, LOW(dtxt)
-	ldi r31, HIGH(dtxt)
-	rjmp displayDString
-
-read:
-	ldi r30, LOW(twoDigit)
-	ldi r31, HIGH(twoDigit)
-	rjmp displayDString
-
-displayDstring:
-	ld r0,Z+
-	tst r0 ; Reached end of message ?
-	breq done_dsd ; Yes => quit
-	swap r0 ; Upper nibble in place
-	out PORTC,r0 ; Send upper nibble out
-	rcall LCDStrobe ; Latch nibble 
-	swap r0 ; Lower nibble in place
-	out PORTC,r0 ; Send lower nibble out
-	rcall LCDStrobe ; Latch nibble
-	rjmp displayDString
-
-done_dsd:
-	ret
-	
+	ret	
 div8u:	
 	sub	drem8u,drem8u	;clear remainder and carry
 	rol	dd8u		;shift left dividend
